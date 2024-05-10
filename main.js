f841981783927e0ef6c1d6d16caef6ff6533977d
Mirae.JK <158263388+javeria-kamran@users.noreply.github.com>
2024-05-10 11:32:07 +0500

    Todo_list

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    let answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in your todo?"
+        }, {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo?",
+            default: false
+        }
+    ]);
+    let { TODO, addmore } = answer;
+    loop = addmore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("Kindly add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your Todo List is:");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No todos found");
+}
